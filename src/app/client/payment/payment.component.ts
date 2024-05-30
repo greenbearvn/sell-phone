a805bf0a87785145212c587cf5dd763d99d179a7
HungVu <hungvu2352002@gmail.com>
2024-05-30 16:57:03 +0700

    fix payment

--- src/app/client/payment/payment.component.ts
+++ src/app/client/payment/payment.component.ts
@@ -1,63 +1,53 @@
 import { Component } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { PaymentService } from 'src/app/services/client/payment/payment.service';
+import { ToastService } from 'angular-toastify';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent {
-
   constructor(
     private paymentService: PaymentService,
     private cookieService: CookieService,
-  ) { }
+    private toastService: ToastService
+  ) {}
 
-  address:any;
-  addressChoice:any;
-  token:any;
+  address: any;
+  addressChoice: any;
+  token: any;
 
   cartData: any[] = [];
 
-  totalMoney:any;
+  totalMoney: any;
 
-  infor:any;
+  infor: any = {
+    customerName: '',
+    address: '',
+    phone: '',
+    orderProductRequestDtos: [],
+    note: '',
+    shippingFee: 0,
+    paymentTypeId: 1,
+  };
 
-
- paymentData:any ={
-  "addressId": 1,
-  "orderProductRequestDtos": [
-    {
-      "productOptionId": 1,
-      "quantity": 2
-    },
-    {
-      "productOptionId": 3,
-      "quantity": 1
+  appendOptions() {
+    for (let item of this.cartData) {
+      this.infor.orderProductRequestDtos.push({
+        productOptionId: item.productOptionDto.id,
+        quantity: item.quantity,
+      });
     }
-  ],
-  "note": "This is a sample note.",
-  "shippingFee": 0,
-  "paymentTypeId": 1
-}
-
-appendOptions() {
-  for (let item of this.cartData) {
-    this.infor.orderProductRequestDtos.push({ 
-      productOptionDto: item.productOptionDto,
-      quantity: item.quantity
-    });
   }
-}
-
 
   ngOnInit() {
     this.getToken();
     this.cartData = JSON.parse(localStorage.getItem('cart') || '[]');
     this.appendOptions();
     this.infor.shippingFee = 0;
-    this.infor.paymentTypeId  = 1;
+    this.infor.paymentTypeId = 1;
     localStorage.clear();
 
     this.getAddressCurrent();
@@ -68,11 +58,12 @@ appendOptions() {
     console.log(this.token);
   }
 
-  getAddressCurrent(){
-    this.paymentService.getAddressCurrentUser(this.token).subscribe((data:any) => {
-      this.address = data.data;
-      
-    });
+  getAddressCurrent() {
+    this.paymentService
+      .getAddressCurrentUser(this.token)
+      .subscribe((data: any) => {
+        this.address = data.data;
+      });
   }
 
   calTotalMoney() {
@@ -83,22 +74,22 @@ appendOptions() {
     }
   }
 
-
   formatCurrencyVND(amount: number): string {
     return amount.toLocaleString('vi-VN', {
       style: 'currency',
       currency: 'VND',
       minimumFractionDigits: 0,
-      maximumFractionDigits: 0
+      maximumFractionDigits: 0,
     });
   }
-
 
   payment() {
-    this.paymentService.payment(this.token,this.infor).subscribe((data:any) => {
-      this.address = data.data;
-      
-    });
-  }
 
+    console.log(this.infor)
+    this.paymentService
+      .payment(this.token, this.infor)
+      .subscribe((data: any) => {
+        this.toastService.success("Quý khách đã mua hàng thành công!!!")
+      });
+  }
 }
